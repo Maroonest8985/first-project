eba3673a3ed0f0d9ce21cbea07753605de5dccfb
maroonest <maroonest98@efit.co.kr>
2024-12-16 17:24:58 +0900

    cors error fix

--- src/config/http.js
+++ src/config/http.js
@@ -4,6 +4,7 @@ const baseURL = axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? '' : process.env.VUE_APP_BASE_API,
     headers: {
         "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Headers': '*',
     }
 });
 
