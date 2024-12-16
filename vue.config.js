f3768720108381cf723adc2200ef488698f61a27
maroonest <maroonest98@efit.co.kr>
2024-12-16 13:25:25 +0900

    Hello world console print

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: "http://138.2.62.193:8080"
+    proxy: "http://138.2.62.193:9091"
   }
 })
