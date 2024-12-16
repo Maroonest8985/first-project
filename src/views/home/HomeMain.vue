675422e9db2fe343f73590e8b19b9f35a184a921
maroonest <maroonest98@efit.co.kr>
2024-12-16 14:15:51 +0900

    dot env add

--- src/views/home/HomeMain.vue
+++ src/views/home/HomeMain.vue
@@ -45,6 +45,7 @@ export default {
   },
   methods: {
     getData(){
+      console.log(process.env.NODE_ENV)
       console.log(process.env.VUE_APP_BASE_API);
 
       baseURL.post('/login', {id : 'asdf', password : 'asdf'}, ).then(res => {
