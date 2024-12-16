d79cb18e6d9f85279b7082c79c4dc8ddb490f312
maroonest <maroonest98@efit.co.kr>
2024-12-16 17:11:35 +0900

    cors error fix

--- src/config/http.js
+++ src/config/http.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const baseURL = axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? '' : process.env.VUE_APP_BASE_API,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    }
 });
 
-
-baseURL.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 export default baseURL;
